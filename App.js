a0fb7cae8641f0b0ddf329fd18ee65198888a6e1
Leroux Antoine <antoine.d.m.leroux@gmail.com>
2021-09-10 17:30:17 +0900

    Added push notification sent directly via the application

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import * as Notifications from "expo-notifications"
 
@@ -12,6 +12,7 @@ Notifications.setNotificationHandler({
 })
 
 export default function App() {
+  const [pushToken, setPushToken] = useState()
   useEffect(() => {
     Notifications.getPermissionsAsync().then(statusObj => {
       if (statusObj.status !== "granted") {
@@ -24,9 +25,10 @@ export default function App() {
       }
     }).then(() => {
       return Notifications.getExpoPushTokenAsync()
-    }).then( data =>
-      console.log(data)
-    ).catch(err => {
+    }).then( response =>{
+      const token = response.data
+      setPushToken(token)
+    }).catch(err => {
       return null
     });
   })
@@ -58,9 +60,27 @@ export default function App() {
     })
   }
 
+
+  const triggerPushNotificationHandler = () => {
+    fetch("https://exp.host/--/api/v2/push/send",{
+      method: "POST",
+      headers: {
+        "Accept": "application/json",
+        "Accept-Encoding": "gzip, deflate",
+        "Content-Type": "application/json"
+      }, 
+      body: JSON.stringify({
+        to: pushToken,
+        data: {extraData : "Some data"},
+        title: "Sent via the app",
+        body: "This push notification was sent via the app"
+      })
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Button title="Trigger notification" onPress={triggerNotificationHandler} />
+      <Button title="Trigger notification" onPress={triggerPushNotificationHandler} />
       <StatusBar style="auto" />
     </View>
   );
